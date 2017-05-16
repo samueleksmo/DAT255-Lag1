61013f4fa775844bb43608d66c8a3b196c28e590
AnneLinnea <anneha@student.chalmers.se>
2017-05-16 16:08:50 +0200

    Lägger till mapp med databas-requests
    Vi måste nog lägga till mongodb i nodemodules i masterbranchen också om dessa filer ska funka, vet inte riktigt hur man gör med zip-filen

---
+++ app/db_requests/deletePortcall.js
@@ -0,0 +1,19 @@
+
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
+
+
+//Tar bort ett portCall, ge portcallid som sträng i metodanrop
+exports.deleteOneItem= function(query) {
+  mongo.connect(url, function(err,db){
+	if (err) throw err;
+  	var myquery = { portCallId: query };
+  	db.collection('ourportcalls').remove(myquery, function(err, obj) {
+    	if (err) throw err;
+    	console.log(obj.result.n + ' document(s) deleted');
+    	db.close();
+	});
+  });
+};
+
