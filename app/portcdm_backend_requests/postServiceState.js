11f075a3fda14d63ff573003105a9bcd5a39e4e2
AnneLinnea <anneha@student.chalmers.se>
2017-05-18 10:55:34 +0200

    Nils fix av xml-koden

--- app/portcdm_backend_requests/postServiceState.js
+++ app/portcdm_backend_requests/postServiceState.js
@@ -3,29 +3,29 @@ const uuidV4 = require('uuid/v4');
 var options = require('./options.js');
 
 exports.postServiceState = function(portCallId, vesselId, timeType, datetime, serviceObject, timeSequence, berth) {
-
-var date = new Date();
-date.setHours(date.getHours - 2);
-
+  var d = new Date();
+d.setHours(d.getHours() - 2);
 var body = '<?xml version="1.0" encoding="UTF-8"?>' +
-'<ns2:portCallMessage xmlns:ns2="urn:x-mrn:stm:schema:port-call-message:0.0.16">' +
-   '<ns2:portCallId>' + portCallId + '</ns2:portCallId>' +
-   '<ns2:vesselId>' + vesselId + '</ns2:vesselId>' +
-   '<ns2:messageId>urn:x-mrn:stm:portcdm:message:' + uuidV4() + '</ns2:messageId>' +
-   '<ns2:reportedAt>' + date + '</ns2:reportedAt>' +
+'<ns2:portCallMessage xmlns:ns2="urn:mrn:stm:schema:port-call-message:0.6">' +
+   '<ns2:portCallId>'+portCallId+'</ns2:portCallId>' +
+   '<ns2:vesselId>'+vesselId+'</ns2:vesselId>' +
+   '<ns2:messageId>urn:mrn:stm:portcdm:message:' + uuidV4() + '</ns2:messageId>' +
+   '<ns2:reportedAt>'+d.toISOString()+'</ns2:reportedAt>' +
    '<ns2:serviceState>' +
-      '<ns2:serviceObject>' + serviceObject + '</ns2:serviceObject>' +
+      '<ns2:serviceObject>'+ serviceObject +'</ns2:serviceObject>' +
       '<ns2:timeSequence>' + timeSequence + '</ns2:timeSequence>' +
       '<ns2:time>' + datetime.toISOString() + '</ns2:time>' +
       '<ns2:timeType>' + timeType + '</ns2:timeType>' +
       '<ns2:at>' +
-        '<ns2:locationType>BERTH</ns2:locationType>' +
-        '<ns2:name>' + berth + '</ns2:name>' +
+        '<ns2:locationMRN>urn:mrn:stm:location:segot:BERTH:'+berth+'</ns2:locationMRN>' +
       '</ns2:at>' +
    '</ns2:serviceState>' +
 '</ns2:portCallMessage>';
-console.dir(body);
-var req = http.request(options.setOptions('/dmp/mss/state_update', 'POST', 'application/xml'), function(res)    {
+
+console.log(body);
+
+
+var req = http.request(options.setOptions('/mb/mss', 'POST', 'application/xml'), function(res)    {
     console.log(res.statusCode);
     console.log(res.statusMessage);
 });
